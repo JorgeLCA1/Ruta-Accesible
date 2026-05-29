fd23c42790e686935c0c84c1cdea43114e183bf5
JorgeLCA1 <l22211533@tijuana.tecnm.mx>
2026-05-28 23:04:03 -0700

    fix: eliminar turbopack config

--- api/next.config.ts
+++ api/next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  allowedDevOrigins: ['172.31.99.222'], 
-  turbopack: {
-    root: ".",
-  },
+  allowedDevOrigins: ['172.31.99.222'],
 };
 
 export default nextConfig;
